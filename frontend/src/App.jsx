09b2114c3e6c9f2f87a5607d5e9e36bd72329097
C <caaarloos300@gmail.com>
2022-08-13 03:36:39 +0200

    Autofetch with backend server for info
    let's gooo! It works , and very fast lol

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import logo from './logo.svg';
-import React, { useState, useEffect, useCallback } from "react";
+import React, { useState, useEffect, useCallback,useRef } from "react";
 import idl1 from './counter.json';
 import * as anchor from "@project-serum/anchor";
 import { Connection, PublicKey, clusterApiUrl, Keypair } from '@solana/web3.js';
@@ -11,7 +11,8 @@ import kp from './keypair.json';
 import './App.css';
 
 function App() {
-  const [todos, setTodos] = useState([])
+const [todos, setTodos] = useState([])
+  
 
   const fetching = async() => {
 
@@ -21,15 +22,50 @@ function App() {
       setTodos(todos)
     
   }
-  useEffect(() => {
-    fetching()
-  }, [])
+
+
+
+  function useInterval(callback, delay) {
+    const savedCallback = useRef();
+  
+    // Remember the latest callback.
+    useEffect(() => {
+      savedCallback.current = callback;
+    }, [callback]);
+  
+    // Set up the interval.
+    useEffect(() => {
+      function tick() {
+        savedCallback.current();
+      }
+      if (delay !== null) {
+        let id = setInterval(tick, delay);
+        return () => clearInterval(id);
+      }
+    }, [delay]);
+  }
+  function Request() {
+    let [requestCount, setRequestCount] = useState(0);
+   
+    // Run every second
+    const delay = 1000;
+  
+    useInterval(() => {
+      // Make the request here
+      
+      fetching()
+      console.log(1);
+    }, delay);
+  
+    return <h1>{requestCount}</h1>;
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          {todos[0]} <code>src/App.js</code> and save to reload.
+        <Request />
         </p>
         <a
           className="App-link"
