ab6c45f9169b151f4881d52cf345a42fd5cdaafb
C <caaarloos300@gmail.com>
2022-08-13 03:06:23 +0200

    client connected with Python server side + fix dependencies

--- QR-Python-API-Darknet/backend-server-FastApi/main.py
+++ QR-Python-API-Darknet/backend-server-FastApi/main.py
@@ -1,6 +1,18 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 import json
 app = FastAPI()
+origins = [
+    "*",
+]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+    expose_headers=["*"])
+
 # instruction to deploy server --> uvicorn main:app --reload
 @app.get("/data")
 async def room():
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,35 @@
 import logo from './logo.svg';
+import React, { useState, useEffect, useCallback } from "react";
+import idl1 from './counter.json';
+import * as anchor from "@project-serum/anchor";
+import { Connection, PublicKey, clusterApiUrl, Keypair } from '@solana/web3.js';
+import { AnchorProvider, Program, Provider, web3, utils } from '@project-serum/anchor';
+import idl2 from './whitelist.json';
+import kp from './keypair.json';
+
+
 import './App.css';
 
 function App() {
+  const [todos, setTodos] = useState([])
+
+  const fetching = async() => {
+
+      const response = await fetch("http://127.0.0.1:8000/data");
+      const todos = await response.json()
+      console.log(todos);
+      setTodos(todos)
+    
+  }
+  useEffect(() => {
+    fetching()
+  }, [])
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          {todos[0]} <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
