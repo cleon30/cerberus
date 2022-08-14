24820af515112e6879eee383c1118213deb119d3
C <caaarloos300@gmail.com>
2022-08-14 18:20:44 +0200

    Update main.py

--- QR-Python-API-Darknet/backend-server-FastApi/main.py
+++ QR-Python-API-Darknet/backend-server-FastApi/main.py
@@ -13,13 +13,6 @@ app.add_middleware(
     allow_headers=["*"],
     expose_headers=["*"])
 
-# instruction to deploy server --> uvicorn main:app --reload
-# @app.get("/data")
-# async def room():
-#     with open('../data.json') as json_file:
-#         data = json.load(json_file)
-#     return data
-
 @app.get("/data/last")
 async def room():
     with open('../data_last.json') as json_file2:
